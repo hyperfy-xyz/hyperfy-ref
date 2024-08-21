8196ae21be97dbfd9f0380a71c95468e995724dd
Ash <hello@ashconnell.com>
2024-08-21 13:24:21 +1000

    [world] worker handoff when tab inactive

--- packages/website/verse/World.js
+++ packages/website/verse/World.js
@@ -65,6 +65,8 @@ export class World extends EventEmitter {
     this.init()
 
     window.world = this
+
+    document.addEventListener('visibilitychange', this.onVisibilityChange)
   }
 
   setAuth(auth) {
@@ -175,7 +177,54 @@ export class World extends EventEmitter {
     this.graphics.renderer.setAnimationLoop(this.tick)
   }
 
+  onVisibilityChange = () => {
+    // if the tab is no longer active, browsers stop triggering requestAnimationFrame.
+    // this is obviously bad because physics stop running and we stop processing websocket messages etc.
+    // instead, we stop using requestAnimationFrame and get a worker to tick at a slower rate using setInterval
+    // and notify us.
+    // this allows us to keep everything running smoothly.
+    // See: https://gamedev.stackexchange.com/a/200503 (kinda fucking genius)
+    if (document.hidden) {
+      // spawn worker if we haven't yet
+      if (!this.worker) {
+        const script = `
+          const rate = 1000 / 5 // 5 FPS, more often than MAX_DELTA_TIME
+          let intervalId = null;
+          self.onmessage = e => {
+            if (e.data === 'start' && !intervalId) {
+              intervalId = setInterval(() => {
+                self.postMessage(1);
+              }, rate);
+              console.log('[worker] tick started')
+            }
+            if (e.data === 'stop' && intervalId) {
+              clearInterval(intervalId);
+              intervalId = null;
+              console.log('[worker] tick stopped')
+            }
+          }
+        `
+        const blob = new Blob([script], { type: 'application/javascript' })
+        this.worker = new Worker(URL.createObjectURL(blob))
+        this.worker.onmessage = () => {
+          const time = performance.now()
+          this.tick(time)
+        }
+      }
+      // stop rAF
+      this.graphics.renderer.setAnimationLoop(null)
+      // tell the worker to start
+      this.worker.postMessage('start')
+    } else {
+      // tell the worker to stop
+      this.worker.postMessage('stop')
+      // resume rAF
+      this.graphics.renderer.setAnimationLoop(this.tick)
+    }
+  }
+
   destroy() {
+    document.removeEventListener('visibilitychange', this.onVisibilityChange)
     this.graphics.renderer.setAnimationLoop(null)
     for (const system of this.systems) {
       system.destroy()
